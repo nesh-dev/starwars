4d60d3781fa0cc112e5c4d20704b8b6c8141710b
nesh-dev <kevinene91@gmail.com>
2021-04-21 11:21:21 +0300

    add pagination

--- packages/client/src/Containers/Home.tsx
+++ packages/client/src/Containers/Home.tsx
@@ -3,6 +3,7 @@ import StarWarsImage from "../assets/Star_Wars_Logo 1.png";
 import styled from "styled-components";
 import SearchBar from "../components/searchBar";
 import PersonCard from "../components/personCard";
+import Pagination from "../components/pagination"; 
 import { useQuery } from "@apollo/client";
 import { GET_STAR_WARS_PEOPLE } from "../graphql/queries/people";
 
@@ -17,13 +18,41 @@ const Logo = styled.img`
   width: 35%;
 `;
 
-const SearchBarWrapper = styled.div`
+const Wrapper = styled.div`
   width: 35%;
   margin-top: 2%;
 `;
 
+const PaginationWrapper = styled(Wrapper)`
+  display: flex; 
+  justify-content: center;
+`; 
+
+
 const Home = () => {
+
   const [pageNumber, setPageNumber] = useState("1");
+  const pageLimit = 9;
+  const myArray= [1,2,3,4,5,6,7,8,9]
+
+  const decrementPage = (e: React.MouseEvent<HTMLButtonElement>): void => { 
+
+    setPageNumber((parseInt(pageNumber) - 1).toString());
+  }
+
+  const incrementPage = (e: React.MouseEvent<HTMLButtonElement>): void=> { 
+    
+    setPageNumber((parseInt(pageNumber) + 1).toString());
+  }
+
+  const getCurrentPageInfo = (e: React.MouseEvent<HTMLButtonElement>, item: number): void => { 
+
+    setPageNumber(item.toString())
+
+  }
+
+
+
   const { data, loading, error } = useQuery(GET_STAR_WARS_PEOPLE, {
     variables: { page: pageNumber },
   });
@@ -34,15 +63,28 @@ const Home = () => {
     <div>
       <Container>
         <Logo src={StarWarsImage} />
-        <SearchBarWrapper>
+        <Wrapper>
           <SearchBar />
-        </SearchBarWrapper>
-        {data &&
+        </Wrapper>
+        { data &&
           data?.people?.results.map((item: any) => (
-            <SearchBarWrapper>
+            <Wrapper>
               <PersonCard key={item.name} people={item} />
-            </SearchBarWrapper>
-          ))}
+            </Wrapper>
+          ))
+          }
+           <PaginationWrapper>
+           <Pagination 
+           pagesInfo={data}
+           myArray={myArray}
+           decrementPage={decrementPage}
+           incrementPage={incrementPage}
+           pageNumber={pageNumber}
+           pageLimit={pageLimit}
+           getCurrentPageInfo={getCurrentPageInfo}
+            />
+           </PaginationWrapper>
+     
       </Container>
     </div>
   );
--- packages/client/src/components/pagination/index.tsx
+++ packages/client/src/components/pagination/index.tsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import KeyLeft from "../../assets/key_arrow_left.png";
+import KeyRight from "../../assets/keyboard_arrow_right 1.png";
 
-export default function index() {
-    return (
-        <div>
-            
-        </div>
-    )
+const PagButton = styled.button<{ active?: boolean }>`
+  background: #ffffff;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  margin-left: 2%;
+  width: 30px;
+  ${(props) =>
+    props.active &&
+    `
+  font-weight: bold;
+  background: #ffe81f;
+  color: #fff;
+`}
+  &:hover {
+    background: #ffe81f;
+    color: #ffff;
+    outline: none;
+    border: none;
+  }
+`;
+
+const PagContainer = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`;
+
+const CountButton = styled(PagButton)`
+  &:hover {
+    background: #ffff;
+    color: #ffff;
+    outline: none;
+    border: 1px solid #ffe81f;
+  }
+`;
+
+const TraverseButton = styled(CountButton)<{ disabled?: boolean }>`
+  ${(props) =>
+    props.disabled &&
+    `
+cursor: none;
+background: #ffff;
+color: #fff;
+border: 1px solid #dddddd;
+&:hover {
+  background: #ffff;
+  color: #ffff;
+  outline: none;
+  border: 1px solid #dddddd;
 }
+`};
+`;
+
+const active = (pageNumber: string, item: number) => {
+  return item === parseInt(pageNumber);
+};
+
+const checkNextDisabled = (pagesInfo: any) => {
+  if (pagesInfo?.people?.next == null) return true;
+};
+
+const checkPrevDisabled = (pagesInfo: any) => {
+  if (pagesInfo?.people?.previous == null) return true;
+};
+const Pagination = ({
+  pagesInfo,
+  myArray,
+  decrementPage,
+  incrementPage,
+  pageNumber,
+  pageLimit,
+  getCurrentPageInfo,
+}: any) => {
+  return (
+    <PagContainer>
+      <TraverseButton
+        onClick={(e) => decrementPage(e)}
+        disabled={checkPrevDisabled(pagesInfo)}
+      >
+        <img src={KeyLeft} alt="key left" />
+      </TraverseButton>
+
+      {myArray &&
+        myArray?.map((item: number, index: number) => (
+          <PagButton
+            active={active(pageNumber, item)}
+            onClick={(e) => getCurrentPageInfo(e, item)}
+          >
+            {item}
+          </PagButton>
+        ))}
+
+      <TraverseButton
+        onClick={(e) => incrementPage(e)}
+        disabled={checkNextDisabled(pagesInfo)}
+      >
+        <img src={KeyRight} alt="key right" />
+      </TraverseButton>
+    </PagContainer>
+  );
+};
+
+export default Pagination;
